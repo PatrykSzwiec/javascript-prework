db89a4e7d3d66a3f0f3d84bbc56ff8daf0fea975
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-03-01 14:05:08 +0100

    Add player move script

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
-
+// COMPUTER MOVE SCRIPT ---------------------------------------------------------
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
@@ -16,3 +16,21 @@ if(randomNumber == 1){
 
 console.log('Ruch komputera to: ' + computerMove);
 printMessage('Ruch komputera to: ' + computerMove);
+// PLAYER MOVE SCRIPT -----------------------------------------------------------
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+    else if (playerInput == '2'){
+        playerMove= 'papier';
+    }
+    else if (playerInput == '3'){
+        playerMove= 'nożyce';
+    }
+console.log('Twój ruch to: ' + playerMove);
+printMessage('Ruch gracza to: ' + playerMove);
