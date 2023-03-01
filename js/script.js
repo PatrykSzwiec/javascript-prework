7ad5cfe8d60d88a27dc8b3b694619e85e94e6b3a
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-03-01 14:40:06 +0100

    Add game result script

--- js/script.js
+++ js/script.js
@@ -1,36 +1,82 @@
+printMessage("<h1>ROCK PAPER SCISSORS GAME</h1>");
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+console.log('The drawn number is : ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = 'unknown move';
 // COMPUTER MOVE SCRIPT ---------------------------------------------------------
 if(randomNumber == 1){
-  computerMove = 'kamień';
+  computerMove = 'rock';
 }
   else if (randomNumber == 2){
-    computerMove = 'papier';
+    computerMove = 'paper';
   }
   else if (randomNumber == 3){
-    computerMove = "nożyce";
+    computerMove = "scissors";
   }
 
-console.log('Ruch komputera to: ' + computerMove);
-printMessage('Ruch komputera to: ' + computerMove);
+console.log('Computer move is : ' + computerMove);
+printMessage('Computer move is : ' + computerMove);
 // PLAYER MOVE SCRIPT -----------------------------------------------------------
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let playerInput = prompt('Make your move ! \n 1: Rock | 2: Paper | 3: Scissors.');
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Player choose : ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = 'unknown move';
 
 if(playerInput == '1'){
-  playerMove = 'kamień';
+  playerMove = 'rock';
 }
     else if (playerInput == '2'){
-        playerMove= 'papier';
+        playerMove= 'paper';
     }
     else if (playerInput == '3'){
-        playerMove= 'nożyce';
+        playerMove= 'scissors';
     }
-console.log('Twój ruch to: ' + playerMove);
-printMessage('Ruch gracza to: ' + playerMove);
+console.log('Player move is : ' + playerMove);
+printMessage('Player move is : ' + playerMove);
+// SETTLEMENT OF THE RESULT
+// computerMove is rock
+if ( computerMove == 'rock' && playerMove == 'paper'){
+    console.log('Player win this round.');
+    printMessage('<h1>Player win this round.</h1>');
+}
+    else if ( computerMove == 'rock' && playerMove == 'rock'){
+        console.log('Its a draw!');
+        printMessage('<h1>Its a draw!</h1>');
+    } 
+    else if ( computerMove == 'rock' && playerMove == 'scissors'){
+        console.log('Computer win this round.');
+        printMessage('<h1>Computer win this round.</h1>');
+    }
+// computerMove is paper
+    else if ( computerMove == 'paper' && playerMove == 'paper'){
+        console.log('Its a draw!');
+        printMessage('<h1>Its a draw!</h1>');
+    }
+    else if ( computerMove == 'paper' && playerMove == 'rock'){
+        console.log('Computer win this round.');
+        printMessage('<h1>Computer win this round.</h1>');
+    } 
+    else if ( computerMove == 'paper' && playerMove == 'scissors'){
+        console.log('Player win this round.');
+        printMessage('<h1>Player win round.</h1>');
+    }
+// computerMove is scissors
+    else if ( computerMove == 'scissors' && playerMove == 'paper'){
+        console.log('Computer win this round.');
+        printMessage('<h1>Computer win this round.</h1>');
+    }
+    else if ( computerMove == 'scissors' && playerMove == 'rock'){
+        console.log('Player win this round.');
+        printMessage('<h1>Player win this round.</h1>');
+    } 
+    else if ( computerMove == 'scissors' && playerMove == 'scissors'){
+        console.log('Its a draw!');
+        printMessage('<h1>Its a draw!</h1>');
+    }
+// playerMove is unknown move
+    else if (computerMove == 'rock' || computerMove == 'paper' || computerMove == 'scissors' && playerMove == 'unknown move'){
+        console.log("Player move isn't correct !!!");
+        printMessage("<h1>Player move isn't correct !!!</h1>");
+    }
