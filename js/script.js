ae1e0937fe3b87061a7a5e5445ea19f3724ed786
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-03-01 13:58:47 +0100

    Add computer move script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+  else if (randomNumber == 2){
+    computerMove = 'papier';
+  }
+  else if (randomNumber == 3){
+    computerMove = "nożyce";
+  }
+
+console.log('Ruch komputera to: ' + computerMove);
+printMessage('Ruch komputera to: ' + computerMove);
